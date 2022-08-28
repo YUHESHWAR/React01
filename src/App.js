c88374f3c5e5d44a777d3d70331c6901f8972f13
YUHESHWAR <yuheshwar.ks@gmail.com>
2022-08-28 23:51:48 +0530

    User images & Msg Component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Msg
+        name="Hinata"
+        image="https://cdn.myanimelist.net/images/characters/11/243919.jpg"
+        position="Middle Blocker"
+      />
+      <Msg
+        name="Kageyama"
+        image="https://i.pinimg.com/originals/90/2b/97/902b97ee34deae638e57d64d3f12bc3d.jpg"
+        position="Setter"
+      />
+      <Msg
+        name="Bokuto"
+        image="https://preview.redd.it/qqz2asvw4aj51.jpg?auto=webp&s=90cd37751ee6e34e99d0c33ce3e193352cd085df"
+        position="Ace"
+      />
+    </div>
+  );
+}
+
+function Msg({ name, image, position }) {
+  return (
+    <div>
+      <img src={image} alt="Img not found" />
+      <h1>{name}</h1>
+      <h2>{position}</h2>
     </div>
   );
 }
