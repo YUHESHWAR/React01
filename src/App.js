c4d5fbdd662875ae6009fbbde77afbc3e56a5051
YUHESHWAR <yuheshwar.ks@gmail.com>
2022-09-03 23:36:28 +0530

    movieList

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
   ]
   return (
     <div className="App">
-      {data.map(({ name, image }) => (
+      {/* {data.map(({ name, image }) => (
         <Welcome name={name} image={image} />
-      ))}
+      ))} */}
 
       {/* <Welcome name="Ajith" />  */}
 
@@ -41,8 +41,101 @@ function App() {
         position="Ace"
       /> */}
 
+      <MovieList />
+
     </div>
   );
+}
+
+function MovieList() {
+  const movieList = [
+    {
+      name: "Vikram",
+      poster: "https://feeds.abplive.com/onecms/images/uploaded-images/2021/07/10/273e787ba809e725948334340c658e68_original.jpg",
+      rating: 8.8,
+      summary: "Agent Vikram (Kamal) with his masked identity leads a masked gang, who have their own tragic story in their past because of a deadly drug mafia. Agent Vikram fights against the drug mafia headed by Santhanam (Vijay Sethupathi) with the help of an undercover agent Amar (Fahad Fasil) and Narein (Bejoy)."
+    },
+    {
+      name: "RRR",
+      poster: "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
+      rating: 6,
+      summary:
+        "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
+    },
+    {
+      name: "Iron man 2",
+      poster: "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+      rating: 7,
+      summary:
+        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+    },
+    {
+      name: "No Country for Old Men",
+      poster: "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+      rating: 8.1,
+      summary:
+        "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+    },
+    {
+      name: "Jai Bhim",
+      poster: "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+      summary:
+        "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
+      rating: 8.9,
+    },
+    {
+      name: "The Avengers",
+      rating: 8,
+      summary:
+        "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+      poster:
+        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+    },
+    {
+      name: "Interstellar",
+      poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
+      rating: 8.6,
+      summary:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
+    },
+    {
+      name: "Baahubali",
+      poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
+      rating: 7.2,
+      summary:
+        "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+    },
+    {
+      name: "Ratatouille",
+      poster:
+        "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
+      rating: 8,
+      summary:
+        "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
+    }
+  ];
+  return <div className='movie-list'>
+    {movieList.map((mv, index) => (
+      <Movies key={index} movie={mv} />
+    ))}
+  </div>
+}
+
+function Movies({ movie }) {
+  const rating_style = {
+    color: movie.rating >= 8 ? "green" : "red"
+  }
+  const [toggle, changeToggle] = useState(false);
+  return <div className='movie-container'>
+    <img src={movie.poster} alt="" className="movie-poster" />
+    <div className="movie-spec">
+      <h2 className="movie-name">{movie.name}</h2>
+      <p style={rating_style} className="movie-rating">⭐{movie.rating}</p>
+    </div>
+    <button onClick={() => changeToggle(!toggle)}>toggle Summary</button>
+    {toggle ? <p className="movie-summary">{movie.summary}</p> : null}
+    <Counter />
+  </div>
 }
 
 function Msg({ name, image, position }) {
@@ -58,7 +151,7 @@ function Msg({ name, image, position }) {
 function Welcome({ name, image }) {
   return (
     <div className='profile-container'>
-      <img src={image} alt="Img not found" />
+      <img className='profile-pic' src={image} alt="Img not found" />
       <h1>{name}</h1>
       <Counter />
     </div>
@@ -69,7 +162,7 @@ function Counter() {
   // var like = 10;
   const [like, setLike] = useState(0);
   const [dislike, setDislike] = useState(0);
-  let likePer = (like / (like + dislike)) * 100
+  let likePer = ((like / (like + dislike)) * 100) || 0
   const styles = {
     color: like > 20 ? "orange" : "blue",
   }
@@ -77,7 +170,7 @@ function Counter() {
     <div>
       {like >= 10 ? <h4 style={styles}>You have won the heart of the ppl❤️😂😊</h4> : null}
       <progress className='progress-bar' id="file" max="100" value={likePer}> {likePer} </progress>
-      <div class="counter-button-container">
+      <div className="counter-button-container">
         <button onClick={() => setLike(like + 1)}> 👍 {like}</button>
         <button onClick={() => setDislike(dislike + 1)}> 👎 {dislike}</button>
       </div>
