2cbebb6b93b07f0c56a34d05d283c6c96351e5e9
YUHESHWAR <yuheshwar.ks@gmail.com>
2023-01-31 00:34:41 +0530

    add movie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Msg } from "./Msg";
 import { MovieList } from "./MovieList";
 import { ColorGame } from "./ColorGame";
+import { useState } from "react";
 
 function App() {
   const users = [
@@ -22,6 +23,55 @@ function App() {
       url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvHTc7edKDzNid7vBPLhiWEp89bPfTNvTd4g&usqp=CAU",
     },
   ];
+  const [movieList, setMovieList] = useState([
+    {
+      name: "Vikram",
+      poster:
+        "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRcywLT8os8ZXJb7n3hmSzrSzpnLIDNgHeIufdEvCprHyuxThul",
+      rating: 8.4,
+      summary:
+        "A special agent investigates a murder committed by a masked group of serial killers. However, a tangled maze of clues soon leads him to the drug kingpin of Chennai. ",
+    },
+    {
+      name: "RRR",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BODUwNDNjYzctODUxNy00ZTA2LWIyYTEtMDc5Y2E5ZjBmNTMzXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg",
+      rating: 7.9,
+      summary:
+        "A fearless revolutionary and an officer in the British force, who once shared a deep bond, decide to join forces and chart out an inspirational path of freedom against the despotic rulers.",
+    },
+    {
+      name: "Bahubali : The beginning",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BYWVlMjVhZWYtNWViNC00ODFkLTk1MmItYjU1MDY5ZDdhMTU3XkEyXkFqcGdeQXVyODIwMDI1NjM@._V1_.jpg",
+      rating: 8.1,
+      summary:
+        "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
+    },
+    {
+      name: "Ant Man",
+      poster: "http://www.movienewsletters.net/photos/208057R1.jpg",
+      rating: 7.1,
+      summary:
+        "Scott, a master thief, gains the ability to shrink in scale with the help of a futuristic suit. Now he must rise to the occasion of his superhero status and protect his secret from unsavoury elements.",
+    },
+    {
+      name: "Black Panther",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_.jpg",
+      rating: 7.3,
+      summary:
+        "After his father's death, T'Challa returns home to Wakanda to inherit his throne. However, a powerful enemy related to his family threatens to attack his nation.",
+    },
+    {
+      name: "Captain America",
+      poster:
+        "https://m.media-amazon.com/images/M/MV5BMTYzOTc2NzU3N15BMl5BanBnXkFtZTcwNjY3MDE3NQ@@._V1_FMjpg_UX1000_.jpg",
+      rating: 6.9,
+      summary:
+        "During World War II, Steve Rogers decides to volunteer in an experiment that transforms his weak body. He must now battle a secret Nazi organisation headed by Johann Schmidt to defend his nation.",
+    },
+  ]);
   return (
     <div className="App">
       {/* <div>
@@ -30,20 +80,51 @@ function App() {
         ))}
       </div> */}
       {/* <ColorGame /> */}
-      <AddMovie />
-      <MovieList />
+      <AddMovie movieList={movieList} setMovieList={setMovieList} />
+      <MovieList movieList={movieList} />
     </div>
   );
 }
 
-function AddMovie() {
+function AddMovie({ movieList, setMovieList }) {
+  const [name, setName] = useState("");
+  const [poster, setPoster] = useState("");
+  const [rating, setRating] = useState("");
+  const [summary, setSummary] = useState("");
   return (
     <div className="add-movie-form">
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Poster" />
-      <input type="text" placeholder="Rating" />
-      <input type="text" placeholder="Summary" />
-      <button>Add Movie</button>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Poster"
+        value={poster}
+        onChange={(event) => setPoster(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Rating"
+        value={rating}
+        onChange={(event) => setRating(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Summary"
+        value={summary}
+        onChange={(event) => setSummary(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          const newMovie = { name, poster, rating, summary };
+          setMovieList([...movieList, newMovie]);
+        }}
+      >
+        Add Movie
+      </button>
     </div>
   );
 }
